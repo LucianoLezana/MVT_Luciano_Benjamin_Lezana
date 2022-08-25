9546ba261c6e872d703ea63dfb6368b2fd0435dc
Luciano Lezana <luchobenjo50@gmail.com>
2022-08-25 14:30:08 -0300

    agregando app familires

--- familiares/models.py
+++ familiares/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+'''Deberá tener un template, una vista y un modelo (como mínimo, pueden usar más)
+La clase del modelo, deberá guardar mínimo un número, una cadena y una fecha (puede guardar más cosas)
+Se deberán crear como mínimo 3 familiares
+Los familiares se deben ver desde la web
+'''
+class Familiares(models.Model):
+    name = models.CharField(max_length=128)
+    date_of_birth = models.DateTimeField() 
+    height = models.DecimalField(max_digits=5, decimal_places=2)
